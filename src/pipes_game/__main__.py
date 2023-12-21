07c8da95876065f8ceb2072cc460d9de3db2b9fc
Lewis Haley <lewis.haley@youview.com>
2023-12-21 02:48:43 +0000

    temp: demonstration of update

--- src/pipes_game/__main__.py
+++ src/pipes_game/__main__.py
@@ -4,6 +4,8 @@ import argparse
 import pathlib
 
 from . import display, drawer, parser
+from .grid import Point
+from .gobject import GLib
 
 
 def parse_args() -> argparse.Namespace:
@@ -42,6 +44,14 @@ def main() -> None:
     game_grid = parser.parse_from_file(args.grid_file)
     frame = drawer.grid_to_frame(game_grid, args.width, args.height)
     display_.update(frame)
+
+    def update():
+        game_grid.set_cell(Point(1, 1), "B")
+        frame = drawer.grid_to_frame(game_grid, args.width, args.height)
+        display_.update(frame)
+
+    GLib.timeout_add_seconds(2, update)
+
     display_.start()
 
 
